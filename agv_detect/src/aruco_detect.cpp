4989862dc54a841ce5d6c662deb072c46050e299
Mincheol710313 <99674522+Mincheol710313@users.noreply.github.com>
2023-08-08 17:18:40 +0900

    Add files via upload

--- agv_detect/src/aruco_detect.cpp
+++ agv_detect/src/aruco_detect.cpp
@@ -11,7 +11,7 @@ class ArucoDetectorROSWrapper
 {
 private:
   std::unique_ptr<ARUCO_DETECTOR> aruco_detector_;
-  image_transport::ImageTransport it_; 
+  image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
   tf::Transform agv_transform_;
@@ -37,21 +37,60 @@ private:
 
 public:
   ArucoDetectorROSWrapper(ros::NodeHandle *nh)
-    : it_(*nh)
+      : it_(*nh)
   {
-    if (!ros::param::get("~squareLength", squareLength_)) { squareLength_ = 0.062;}
-    if (!ros::param::get("~markerLength", markerLength_)) { markerLength_ = 0.0496;}
-    if (!ros::param::get("~showRejected", showRejected_)) { showRejected_ = "false";}
-    if (!ros::param::get("~estimatePose", estimatePose_)) { estimatePose_ = "/home/godssi/catkin_ws/src/agv_detect/include/calibration_540.yml";}
-    if (!ros::param::get("~autoScale", autoScale_)) { autoScale_ = "";}
-    if (!ros::param::get("~markerDistance", markerDistance_)) { markerDistance_ = 1.8;}
-    if (!ros::param::get("~multipleMarkers", multipleMarkers_)) { multipleMarkers_ = true;}
-    if (!ros::param::get("~detectorParams", detectorParams_)) { detectorParams_ = "";}
-    if (!ros::param::get("~refine", refine_)) { refine_ = 2;}
-    if (!ros::param::get("~camId", camId_)) { camId_ = 4;}
-    if (!ros::param::get("~video", video_)) { video_ = "";}
-    if (!ros::param::get("~dictionary", dictionary_)) { dictionary_ = 3;}
-    if (!ros::param::get("~custom_dictionary", custom_dictionary_)) { custom_dictionary_ = "";}
+    if (!ros::param::get("~squareLength", squareLength_))
+    {
+      squareLength_ = 0.062;
+    }
+    if (!ros::param::get("~markerLength", markerLength_))
+    {
+      markerLength_ = 0.0496;
+    }
+    if (!ros::param::get("~showRejected", showRejected_))
+    {
+      showRejected_ = "false";
+    }
+    if (!ros::param::get("~estimatePose", estimatePose_))
+    {
+      estimatePose_ = "/home/godssi/catkin_ws/src/agv_detect/include/calibration_540.yml";
+    }
+    if (!ros::param::get("~autoScale", autoScale_))
+    {
+      autoScale_ = "";
+    }
+    if (!ros::param::get("~markerDistance", markerDistance_))
+    {
+      markerDistance_ = 1.8;
+    }
+    if (!ros::param::get("~multipleMarkers", multipleMarkers_))
+    {
+      multipleMarkers_ = true;
+    }
+    if (!ros::param::get("~detectorParams", detectorParams_))
+    {
+      detectorParams_ = "";
+    }
+    if (!ros::param::get("~refine", refine_))
+    {
+      refine_ = 2;
+    }
+    if (!ros::param::get("~camId", camId_))
+    {
+      camId_ = 4;
+    }
+    if (!ros::param::get("~video", video_))
+    {
+      video_ = "";
+    }
+    if (!ros::param::get("~dictionary", dictionary_))
+    {
+      dictionary_ = 3;
+    }
+    if (!ros::param::get("~custom_dictionary", custom_dictionary_))
+    {
+      custom_dictionary_ = "";
+    }
 
     aruco_detector_.reset(new ARUCO_DETECTOR(squareLength_, markerLength_, showRejected_, estimatePose_, autoScale_, markerDistance_, multipleMarkers_, detectorParams_, refine_, camId_, video_, dictionary_, custom_dictionary_));
 
@@ -63,7 +102,7 @@ public:
     marker_pos_pub_ = nh->advertise<geometry_msgs::Pose2D>("/marker_pos", 10);
   }
 
-  void callbakPosition(const sensor_msgs::ImageConstPtr& msg)
+  void callbakPosition(const sensor_msgs::ImageConstPtr &msg)
   {
     cv_bridge::CvImagePtr cv_ptr;
     try
@@ -77,11 +116,11 @@ public:
     }
 
     // 받아온 이미지를 통해서 pose를 계산하는 코드
-    vector< Vec3d > rvecs_, tvecs_;
-    vector< int > markerIds_;
-    vector< Vec4i > diamondIds_;
-    vector< vector< Point2f > > markerCorners_, rejectedMarkers_, diamondCorners_;
-    vector< Vec3d > camPos_;
+    vector<Vec3d> rvecs_, tvecs_;
+    vector<int> markerIds_;
+    vector<Vec4i> diamondIds_;
+    vector<vector<Point2f>> markerCorners_, rejectedMarkers_, diamondCorners_;
+    vector<Vec3d> camPos_;
     Vec3d camPosMean_;
     Mat camRot_;
     double camYaw_;
@@ -130,62 +169,65 @@ public:
 
     // marker_pos_pub_.publish(marker_pos);
     // marker_pos_pub_.publish(marker_pos);
-    
+
     // // camera_tf를 publish하는 코드
-    static tf::TransformBroadcaster br;
-    tf::Transform camera_transform_;
-    camera_transform_.setOrigin( tf::Vector3(camera_pose_.x, camera_pose_.y, 0.0) );
-    tf::Quaternion q;
-    q.setRPY(0, 0, camera_pose_.theta);
-    camera_transform_.setRotation(q);
-    br.sendTransform(tf::StampedTransform(camera_transform_, ros::Time::now(), "world", "camera_tf"));
+    if (camera_pose_.x != 0 && camera_pose_.y != 0)
+    {
+      static tf::TransformBroadcaster br;
+      tf::Transform camera_transform_;
+      camera_transform_.setOrigin(tf::Vector3(camera_pose_.x, camera_pose_.y, 0.0));
+      tf::Quaternion q;
+      q.setRPY(0, 0, camera_pose_.theta);
+      camera_transform_.setRotation(q);
+      br.sendTransform(tf::StampedTransform(camera_transform_, ros::Time::now(), "world", "camera_tf"));
 
-    // agv_tf를 publish하는 코드
-    static tf::TransformBroadcaster br2;
-    tf::Transform agv_transform_;
-    agv_transform_.setOrigin( tf::Vector3(-0.4, -0.0325, 0.0) );
-    tf::Quaternion q2;
-    q2.setRPY(0, 0, 0);
-    agv_transform_.setRotation(q2);
-    br2.sendTransform(tf::StampedTransform(agv_transform_, ros::Time::now(), "camera_tf", "agv_tf"));
+      // agv_tf를 publish하는 코드
+      static tf::TransformBroadcaster br2;
+      tf::Transform agv_transform_;
+      agv_transform_.setOrigin(tf::Vector3(-0.4, -0.0325, 0.0));
+      tf::Quaternion q2;
+      q2.setRPY(0, 0, 0);
+      agv_transform_.setRotation(q2);
+      br2.sendTransform(tf::StampedTransform(agv_transform_, ros::Time::now(), "camera_tf", "agv_tf"));
 
-    // marker_pos를 publish하는 코드
-    tf::StampedTransform transform;
-    try{
-       if(camera_pose_.x == 0 && camera_pose_.y == 0){
-        geometry_msgs::Pose2D marker_pos;
-        marker_pos.x = 0;
-        marker_pos.y = 0;
-        marker_pos.theta = 0;
-        marker_pos_pub_.publish(marker_pos);
-      }
-      else{
-        listener.waitForTransform("world", "agv_tf", ros::Time(0), ros::Duration(0));
+      // marker_pos를 publish하는 코드
+      tf::StampedTransform transform;
+      try
+      {
+        listener.waitForTransform("world", "agv_tf", ros::Time(0), ros::Duration(3.0));
         listener.lookupTransform("world", "agv_tf", ros::Time(0), transform);
-      
+
         // 불러온 pose 값을 geometry_msgs::Pose2D 형태로 변환합니다.
-        geometry_msgs::Pose2D marker_pos; 
+        geometry_msgs::Pose2D marker_pos;
         marker_pos.x = transform.getOrigin().x();
         marker_pos.y = transform.getOrigin().y();
         marker_pos.theta = tf::getYaw(transform.getRotation());
 
-        if(marker_pos.x <= -0.4 || marker_pos.x <= -0.0325 || marker_pos.y <= -0.4 || marker_pos.y <= -0.0325){
-          marker_pos.x = 0;
-          marker_pos.y = 0;
-          marker_pos.theta = 0;
-        }
+        // if(marker_pos.x <= -0.4 || marker_pos.x <= -0.0325 || marker_pos.y <= -0.4 || marker_pos.y <= -0.0325){
+        //   marker_pos.x = 0;
+        //   marker_pos.y = 0;
+        //   marker_pos.theta = 0;
+        // }
 
         // 변환한 pose 값을 /agv_pose로 publish합니다.
         marker_pos_pub_.publish(marker_pos);
       }
+      catch (tf::TransformException &ex)
+      {
+        ROS_ERROR("%s", ex.what());
+      }
     }
-    catch (tf::TransformException &ex) {
-      ROS_ERROR("%s",ex.what());
+    else{
+      geometry_msgs::Pose2D marker_pos;
+      marker_pos.x = 0;
+      marker_pos.y = 0;
+      marker_pos.theta = 0;
+      marker_pos_pub_.publish(marker_pos);
     }
   }
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "aruco_detect");
   ros::NodeHandle nh;
@@ -198,4 +240,3 @@ int main(int argc, char** argv)
 
   ros::waitForShutdown();
 }
-
